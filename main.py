a20a7975dff1dd594b3b1e56c7383c936c99e5ec
scutLaoYi <scutlaoyi@gmail.com>
2016-05-14 09:30:48 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import io
+import sys
+import json
+import argparse
+import logging
+import time
+import threading
+import queue
+
+logging.basicConfig(format='[%(levelname)s][%(asctime)s] %(message)s', level=logging.DEBUG)
+
+
+def main():
+    logging.info("[main] Engine Start!")
+    logging.info("[main] Exit!")
+
+if __name__ == "__main__":
+    main()
