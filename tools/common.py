7e4e32dc07e49d0e51bd43db5e54303cb709ce90
scutLaoYi <scutlaoyi@gmail.com>
2016-05-15 09:58:09 +0800

    calculator for top char in text ok!

--- tools/common.py
+++ tools/common.py
@@ -7,5 +7,42 @@ import logging
 import time
 import threading
 import queue
+import string
 
 logging.basicConfig(format='[%(levelname)s][%(asctime)s] %(message)s', level=logging.DEBUG)
+
+def calculate_chars(text):
+    char_sum = {}
+    top_count = 0
+    top_char = None
+    for c in text:
+        if c not in string.ascii_lowercase:
+            continue
+        if c in char_sum:
+            char_sum[c] += 1
+        else:
+            char_sum[c] = 1
+        if top_count < char_sum[c]:
+            top_count = char_sum[c]
+            top_char = c
+    logging.info("[COMMON TOOL][CHAR CALCULATE] found top char {}, {} times".format(top_char, top_count))
+    return top_char
+
+def calculate_chars_with_group(text, group_size):
+    text_list = []
+    for i in range(0, group_size):
+        text_list.append("")
+    counter = 0
+    for c in text:
+        if c not in string.ascii_lowercase:
+            continue
+        text_list[counter] += c
+        counter += 1
+        counter %= group_size
+    top_char_list = []
+    for i in range(0, group_size):
+        top_char_list.append(calculate_chars(text_list[i]))
+    return top_char_list
+
+
+
